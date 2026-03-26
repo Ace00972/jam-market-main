4641f54203fc5c94907d258d384e99bfe92271a1
Ace00972 <dravidwright00@gmail.com>
2026-03-26 18:37:04 -0500

    update

--- server/db.js
+++ server/db.js
@@ -5,10 +5,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host:               process.env.DB_HOST     || 'localhost',
+  host:               process.env.DB_HOST     || 'gondola.proxy.rlwy.net',
+  port:               process.env.DB_PORT     || 26263, 
   user:               process.env.DB_USER     || 'root',
-  password:           process.env.DB_PASSWORD || '',
-  database:           process.env.DB_NAME     || 'jammarket_db',
+  password:           process.env.DB_PASSWORD || 'zRTMsmsDrhMVWZjHPZeVOZjgPFvQwECu',
+  database:           process.env.DB_NAME     || 'railway',
   waitForConnections: true,
   connectionLimit:    10,
 });
