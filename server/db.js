11903d80bff4c95e703f133c3793c0ae5f3da936
Ace00972 <dravidwright00@gmail.com>
2026-03-26 21:10:56 -0500

    update

--- server/db.js
+++ server/db.js
@@ -1,17 +1,12 @@
 // ════════════════════════════════
 //  JAM MARKET — server/db.js
 // ════════════════════════════════
-const mysql = require('mysql2/promise');
-require('dotenv').config();
-
 const pool = mysql.createPool({
-  host:               process.env.DB_HOST     || 'gondola.proxy.rlwy.net',
-  port:               process.env.DB_PORT     || 26263, 
-  user:               process.env.DB_USER     || 'root',
-  password:           process.env.DB_PASSWORD || 'zRTMsmsDrhMVWZjHPZeVOZjgPFvQwECu',
-  database:           process.env.DB_NAME     || 'railway',
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit:    10,
+  connectionLimit: 10,
 });
-
-module.exports = pool;
